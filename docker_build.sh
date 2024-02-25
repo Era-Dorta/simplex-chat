9f827db6e3c5bc1069b4e86de059ae8b22be2433
Era Dorta <era.dorta@proton.me>
2024-02-25 17:02:53 +0100

    Hotfix for docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build --progress plain --tag eradorta/simplex-chat:bot .
+# Docker v23 builds with buildx automatically but I'm getting no internet during the building process
+# docker build --progress plain --tag eradorta/simplex-chat:bot .
+
+DOCKER_BUILDKIT=0 docker build --tag eradorta/simplex-chat:bot .
