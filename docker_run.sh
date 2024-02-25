9ccbcfa370591dc0ca1de0be0522a510d97d91e5
Era Dorta <era.dorta@proton.me>
2024-02-25 17:21:03 +0100

    Add more intructions to docker run script

--- docker_run.sh
+++ docker_run.sh
@@ -3,3 +3,9 @@
 docker run -it \
 --mount type=bind,source="$(pwd)",target=/project \
 eradorta/simplex-chat:bot bash
+
+
+# After running, execute the following
+# cabal update
+# cabal run simplex-bot-advanced
+# cabal run simplex-anonymous-broadcast-bot
